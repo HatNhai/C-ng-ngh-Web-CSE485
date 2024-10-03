b910a4c0f7651244e16686807fa1fbc021e3c57a
HatNhai <nhaivu.2004@gmail.com>
2024-10-03 17:19:54 +0700

    Install Pest

--- database/migrations/2024_10_03_101659_authors.php
+++ database/migrations/2024_10_03_101659_authors.php
@@ -12,10 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('authors', function (Blueprint $table){
-            $table->id
-
-
-
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
         });
     }
 
